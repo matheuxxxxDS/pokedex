5b511620226510f2ff220380d8d1af73c77e7b1b
MatheuxxxxDS <matheushe94@gmail.com>
2024-03-13 22:26:39 -0300

    ACABAR POKEDEX PAGINA 146 ONCLICK FEITO

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -32,21 +32,28 @@ public class HomeController : Controller
         ViewData["Tipos"] = tipos;
         return View(pokemons);
     }
-
-
     public IActionResult Details(int id)
     {
+        List<Pokemon> pokemons = [];
        using(StreamReader leitor = new("Data\\pokemons.json"))
         {
             string dados = leitor.ReadToEnd();
             pokemons = JsonSerializer.Deserialize <List<Pokemon>>(dados);
         }
-        var pokemon = pokemons
-        .Where(p => p.Numero == id)
-        .FirstOrDefault();
-        return View(pokemon);
+        List<Tipo> tipos = [];
+        using(StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize <List<Tipo>>(dados);
+        }
+        DetailsVM details = new() {
+            Tipos = tipos,
+            Atual = pokemons.FirstOrDefault(p => p.Numero == id),
+            Anterior = pokemons.OrderByDescending(p => p.Numero == id).FirstOrDefault(p => p.Numero < id),
+            Proximo = pokemons.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero > id),
+        };
+        return View(details);
     }
-
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
